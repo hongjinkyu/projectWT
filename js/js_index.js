146dc2d986c53c62774e7f5cf9492b05a48039e6
SoRaang <dsc0320@hotmail.com>
2021-09-30 18:01:46 +0900

    Alpha 0.02

--- js/js_index.js
+++ js/js_index.js
@@ -2,6 +2,22 @@
 
 $(document).ready(function() { // jQuery Load
 
-
+    $('.slider_main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        arrows: true,
+        dots: true,
+        appendArrows: $('#mainSlider'),
+        appendDots: $('#mainSlider'),
+        draggable: true,
+        swipe: true,
+        swipeToSlide: true,
+        touchMove: true,
+        zIndex: 1
+    });
 
 }) // jQuery Closed
