1236463464497d0e962cf6813bb44c18d60877f7
SoRaang <dsc0320@hotmail.com>
2021-10-14 16:51:30 +0900

    Alpha 0.097

--- resources/gathering/js/js_gathering_open.js
+++ resources/gathering/js/js_gathering_open.js
@@ -6,6 +6,22 @@ $(document).ready(function() {
 
     }); // Window Scrolling Actions
 
+    $('#summernote').summernote({
+        placeholder: '어쩌고 저쩌고 에디터',
+        lang: 'ko-KR',
+        tabsize: 2,
+        minHeight: 300,
+        maxHeight: 800,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['insert', ['link', 'picture', 'video']],
+            ['view', ['fullscreen', 'codeview', 'help']]
+        ]
+    });
+
 // ---------- Actual Codes ----------
 
 }) // jQuery Closed
