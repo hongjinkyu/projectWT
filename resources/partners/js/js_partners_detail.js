c42fc09657edc88e91dcef7eeb5b3679ad95d190
SoRaang <dsc0320@hotmail.com>
2021-11-11 16:08:47 +0900

    minor patch

--- resources/partners/js/js_partners_detail.js
+++ resources/partners/js/js_partners_detail.js
@@ -5,16 +5,20 @@ $(document).ready(function () { // jQuery Load
     $(window).scroll(function () {
         var scroll_distance = $(window).scrollTop();
 
-        if (scroll_distance < $('#detailLocation').offset().top) {
+        if (scroll_distance < $('#partnerMenu').offset().top) {
             $('.side_mnu_item1').addClass('checked').siblings('li').removeClass('checked');
         }
 
-        else if (scroll_distance >= $('#detailLocation').offset().top && scroll_distance < $('#detailReview').offset().top) {
+        else if (scroll_distance >= $('#partnerMenu').offset().top && scroll_distance < $('#partnerReply').offset().top) {
             $('.side_mnu_item2').addClass('checked').siblings('li').removeClass('checked');
         }
 
-        else if (scroll_distance >= $('#detailReview').offset().top) {
+        else if (scroll_distance >= $('#partnerReply').offset().top && scroll_distance < $('#partnerReview').offset().top) {
             $('.side_mnu_item3').addClass('checked').siblings('li').removeClass('checked');
+        }
+
+        else if (scroll_distance >= $('#partnerReview').offset().top) {
+            $('.side_mnu_item4').addClass('checked').siblings('li').removeClass('checked');
         }
     }); // Window Scrolling Actions
 
