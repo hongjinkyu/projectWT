3432ccf81ce3a745571d344ce42e72cbf22c1281
SoRaang <dsc0320@hotmail.com>
2021-12-14 17:38:01 +0900

    minor patch

--- resources/common/js/js_mypage.js
+++ resources/common/js/js_mypage.js
@@ -8,4 +8,15 @@ $(document).ready(function() {
 
 // ---------- Actual Codes ----------
 
+    $('.tab_buttons > p').each(function() {
+        $(this).click(function() {
+            let tab_target = $('.' + $(this).attr('data-target'));
+
+            $(this).addClass('selected');
+            $(this).siblings().removeClass('selected');
+            tab_target.addClass('show');
+            tab_target.siblings().removeClass('show');
+        });
+    });
+
 }) // jQuery Closed
