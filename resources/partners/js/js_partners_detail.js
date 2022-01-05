56dc473dc431c0699a6a3d6c860a94928cde761e
SoRaang <dsc0320@hotmail.com>
2022-01-05 11:12:14 +0900

    partners new on real page

--- resources/partners/js/js_partners_detail.js
+++ resources/partners/js/js_partners_detail.js
@@ -1,57 +1,81 @@
-// ---------- Page-Specific jQuery (Index) ----------
+// ---------- Page-Specific jQuery (Patners - Detail) ----------
 
-$(document).ready(function () { // jQuery Load
+$(document).ready(function() { // jQuery Load
 
-    $(window).scroll(function () {
-        var scroll_distance = $(window).scrollTop() + ($(window).height() / 3);
-
-        if (scroll_distance < $('#partnerMenu').offset().top) {
-            $('.side_mnu_item1').addClass('checked').siblings('li').removeClass('checked');
-        }
-
-        else if (scroll_distance >= $('#partnerMenu').offset().top && scroll_distance < $('#partnerReview').offset().top) {
-            $('.side_mnu_item2').addClass('checked').siblings('li').removeClass('checked');
-        }
-
-        else if (scroll_distance >= $('#partnerReview').offset().top && scroll_distance < $('#partnerLocation').offset().top) {
-            $('.side_mnu_item3').addClass('checked').siblings('li').removeClass('checked');
-        }
-
-        else if (scroll_distance >= $('#partnerLocation').offset().top) {
-            $('.side_mnu_item4').addClass('checked').siblings('li').removeClass('checked');
-        }
-    }); // Window Scrolling Actions
-
-    $(window).resize(function() {
-        if ($(window).width() <= 400) {
-            $('#btnOpen').addClass('mobile');
-            $('#detailSlide').addClass('mobile');
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 400) {
+            $('.detail_hiddentitle').slideDown(150);
+            // $('.detail_hiddenbutton').fadeIn(150);
         } else {
-            $('#btnOpen').removeClass('mobile');
-            $('#detailSlide').removeClass('mobile');
+            $('.detail_hiddentitle').slideUp(150);
+            // $('.detail_hiddenbutton').fadeOut(150);
         }
     });
 
     // ---------- Actual Codes ----------
 
-    $('.slide_body').slick({
-        slide: 'img',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        arrows: false,
-        dots: false,
-        asNavFor: '.slide_thumbs',
+    $('.starpoint_selector').hover(function() {
+        $('.count_review').fadeToggle(150);
     });
 
-    $('.slide_thumbs').slick({
-        slide: '.btn_thumb',
-        variableWidth: true,
-        slidesToShow: 5,
+    $('.star_single').each(function() {
+        $(this).hover(function() {
+            $(this).toggleClass('glow');
+            $(this).prevAll().toggleClass('glow');
+        });
+
+        $(this).click(function() {
+            $(this).addClass('decided');
+            $(this).prevAll().addClass('decided');
+            $(this).nextAll().addClass('star_blank');
+
+            let sp_count = $('.star_single.decided').length;
+
+            $('#hiddenSPCounter').val(sp_count);
+            $('.starpoint_mine > span').text(sp_count);
+
+            $('.starpoint_total').fadeOut(150);
+            $('.starpoint_mine').fadeIn(150);
+        });
+    });
+
+    $('.button_container > button').each(function() {
+        $(this).click(function() {            
+            $(this).siblings().removeClass('tab_selected');
+            $(this).addClass('tab_selected');
+
+            let tab_href = $(this).attr('data-tab-target');
+
+            $('.course_single_tab').removeClass('show');
+            $('.' + tab_href + '').addClass('show');
+        });
+    });
+
+    $('.mobile_tab_buttons > button').each(function() {
+        $(this).click(function() {            
+            $(this).siblings().removeClass('tab_selected');
+            $(this).addClass('tab_selected');
+
+            let tab_href = $(this).attr('data-tab-target');
+
+            $('.mobile_single_tab').removeClass('show');
+            $('.' + tab_href + '').addClass('show');
+        });
+    });
+
+    setInterval(function() {
+        $('.grid_item').last().hide().prependTo($('#mobileMainIMG')).fadeIn(300);
+    }, 3000);
+
+    $('.detail_slider').slick({
+        slide: '.slider_item',
+        slidesToShow: 1,
         slidesToScroll: 1,
-        draggable: false,
-        focusOnSelect: true,
-        asNavFor: '.slide_body',
+        autoplay: false,
+        infinite: true,
+        fade: true,
+        dots: false,
+        arrows: false,
     });
 
 }) // jQuery Closed
